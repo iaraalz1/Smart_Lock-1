7c751fbc3e47c9f3560f7d7f37860da50b88c7bc
alanjtoledob <alanjoelcapo@gmail.com>
2023-08-19 18:01:47 -0300

    agregar datos a la tabla de USER

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->foreignId('current_team_id')->nullable();
-            $table->string('profile_photo_path', 2048)->nullable();
+            $table->string('rol')->nullable(); // Ahora es nullable
+            $table->string('rfid')->nullable();
             $table->timestamps();
         });
     }
