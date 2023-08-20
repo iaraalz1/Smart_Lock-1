3f59d14b64840b2b0919ec2357e2aa0e9bfa2e95
alanjtoledob <alanjoelcapo@gmail.com>
2023-08-20 00:10:33 -0300

    haciendo la plantilla para acceder al header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,83 +1,12 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menu desplegable</title>
-    <link rel="stylesheet" href="css/estilos.css">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 </head>
 <body>
-    <nav class="menu">
-        <section class="menu__container">
-            <h1 class="menu__logo">Smart Lock</h1>
-
-            <ul class="menu__links">
-                <li class="menu__item">
-                    <a href="#" class="menu__link">Home</a>
-                </li>
-    
-                <li class="menu__item menu__item--show">
-                    <a href="#" class="menu__link">About <img src="assets/arrow.svg" class="menu__arrow"></a>
-    
-                    <ul class="menu__nesting">
-                        <li class="menu__inside">
-                            <a href="#" class="menu__link menu__link--inside">About 1</a>
-                        </li>
-                        <li class="menu__inside">
-                            <a href="#" class="menu__link menu__link--inside">About 2</a>
-                        </li>
-                        <li class="menu__inside">
-                            <a href="#" class="menu__link menu__link--inside">About 3</a>
-                        </li>
-                    </ul>
-                </li>
-    
-                <li class="menu__item  menu__item--show">
-                    <a href="#" class="menu__link">Projects  <img src="assets/arrow.svg" class="menu__arrow"></a>
-    
-                    <ul class="menu__nesting">
-                        <li class="menu__inside">
-                            <a href="#" class="menu__link menu__link--inside">Projects 1</a>
-                        </li>
-                        <li class="menu__inside">
-                            <a href="#" class="menu__link menu__link--inside">Projects 2</a>
-                        </li>
-                        <li class="menu__inside">
-                            <a href="#" class="menu__link menu__link--inside">Projects 3</a>
-                        </li>
-                    </ul>
-                </li>
-    
-                <li class="menu__item">
-                    <a href="#" class="menu__link">Contact</a>
-                </li>
-
-                <li class="menu__item">
-                    @if (Route::has('login'))
-                    <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-                        @auth
-                            <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
-                        @else
-                            <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
-            
-                            @if (Route::has('register'))
-                                <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                            @endif
-                        @endauth
-                    </div>
-                @endif
-                </li>
-            </ul>
-
-            <div class="menu__hamburguer">
-                <img src="assets/menu.svg" class="menu__img">
-            </div>
-        </section>
-
-
-    </nav>
-
-    <script src="js/app.js"></script>
+    @include('header')
 </body>
 </html>
