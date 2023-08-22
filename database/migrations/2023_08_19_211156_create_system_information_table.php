f54ffeb05a6079afc0f46c64cab7f54fac08b3b7
iaraalz1 <lopezzaupaiara@gmail.com>
2023-08-22 15:07:20 -0300

    creando migraciones

---
+++ database/migrations/2023_08_19_211156_create_system_information_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('system_information', function (Blueprint $table) {
+            $table->id();
+            $table->string('time', 10);
+            $table->integer('state');
+            $table->timestamps();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('system_information');
+    }
+    
+};
